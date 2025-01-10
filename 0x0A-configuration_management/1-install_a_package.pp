1a33426763a747b0eb9eb236cedd2c82132db016
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-10 15:30:52 +0200

    Follow puppet-lint rules

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
-/* Using Puppet, install flask from pip3.
-
-Requirements:
-
-- Install flask
-- Version must be 2.1.0 */
+# Using Puppet, install flask from pip3.
+#
+# Requirements:
+#
+# - Install flask
+# - Version must be 2.1.0
 
 package { 'Flask':
+  ensure   => '2.1.0'
   provider => 'pip3',
-  ensure => "2.1.0"
 }
