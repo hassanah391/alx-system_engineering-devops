96b6ff03cff9640266da62f27727fb42c42ecde8
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-11 12:12:32 +0200

    A puppet file set up your client SSH configuration file so that you can connect to a server without typing a password.

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# A puppet config file to set up your client SSH configuration file
+# so that you can connect to a server without typing a password.
+#
+# Requirements:
+# - must be configured to use the private key ~/.ssh/school
+# - must be configured to refuse to authenticate using a password
+file {'~/.ssh/config':
+  ensure => file,
+  mode => 0600,
+  content => @("EOF")
+Host alx_server
+    HostName 100.26.246.61
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+  | EOF
+}
