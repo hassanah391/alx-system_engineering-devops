62b90c963a3d6af16b90f820dee4c541d0d7f9d0
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-02-25 20:47:17 +0200

    Handles possible request failures

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-Retuens number of subscribers for a passed subreddit name using reddit API
+Returns the number of subscribers for a given subreddit using the Reddit API.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Retuens number of subscribers for a passed subreddit name using reddit API
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+    If an invalid subreddit is given, returns 0.
     """
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
+    URL = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyUserAgent/1.0'}
 
-    req = requests.get(URL, headers=headers).json()
-    subscribers = req.get('data', {}).get('subscribers')
-    if not subscribers:
+    response = requests.get(URL, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
 
-
-    return subscribers
+    data = response.json()
+    return data.get('data', {}).get('subscribers', 0)
